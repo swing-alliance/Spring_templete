7b35d21f2402b5d1d0c4f7d3454589d9193489b7
zyh <zyh496100221@163.com>
2026-06-23 21:50:28 +0800

    模板

--- src/main/java/com/personal/main/controller/UserAuth.java
+++ src/main/java/com/personal/main/controller/UserAuth.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,41 +40,26 @@ public class UserAuth {
     public ResponseEntity<String> login(@RequestBody LoginRequest loginReq, HttpServletResponse response) {
         try {
             User user = authService.verifyLogin(loginReq);
-            
-            // 生成随机 Session Token
             String randomToken = UUID.randomUUID().toString();
             authService.saveUserSession(randomToken, user);
-            
-            // 下发 Cookie
             Cookie cookie = new Cookie("user_session", randomToken);
             cookie.setMaxAge(3600);
             cookie.setHttpOnly(true);
             cookie.setPath("/");
             response.addCookie(cookie);
             return ResponseEntity.ok("登录成功！当前用户账号: " + user.getAccountId());
-            
         } catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
         }
     }
-     @GetMapping("/mainpage")
-    public String mainPage(@CookieValue(value = "user_session", defaultValue = "") String token) {
-        try {
-            Long userId = authService.authCookie(token);
-            return "欢迎访问主页面！当前用户 ID: " + userId;
-        } catch (RuntimeException e) {
-            return e.getMessage();
-        }
-    }
+    
     @PostMapping("/register")
     public String register(@RequestBody RegisterRequest registerReq,HttpServletResponse response) {
-        // 1. 先调用 AuthController 的注册方法，完成用户注册
         User registuser = authService.registerUser(registerReq);
-        // 2. 注册成功后，直接调用登录方法，完成自动登录
         if (registuser != null) {
             return "注册成功！";
         } else {
-            return "注册失败！"; // 注册失败，直接返回错误消息
+            return "注册失败！";
         }
     }
 }
--- src/main/java/com/personal/main/repository/UserRepository.java
+++ src/main/java/com/personal/main/repository/UserRepository.java
@@ -2,11 +2,9 @@ package com.personal.main.repository;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository; // 切换为传统 CRUD 接口
-import org.springframework.stereotype.Repository;
 
 import com.personal.main.model.User;
 
-@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
